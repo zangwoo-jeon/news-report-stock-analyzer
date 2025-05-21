b9711db07bbcc096c9cc66b2fa4adfa85adc6d17
hyeongjun <gudwns1423@gmail.com>
2025-05-22 00:53:03 +0900

    초기 세팅

---
+++ investinghelper/src/test/java/com/investinghelper/InvestinghelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investinghelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestinghelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- investinghelper/src/test/java/com/investinghelper/global/kis/KisTokenTest.java
+++ investinghelper/src/test/java/com/investinghelper/global/kis/KisTokenTest.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class KisTokenTest {
