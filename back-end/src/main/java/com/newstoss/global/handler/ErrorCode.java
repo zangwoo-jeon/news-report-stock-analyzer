a08e997313497edc9b22b1c82ab515f14d0e1c46
parkstar12 <zzanggu12280@naver.com>
2025-05-22 16:19:44 +0900

    [FIX] : 예외처리 수정

---
+++ back-end/src/main/java/com/newstoss/global/handler/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.newstoss.global.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, false, "USER-001", "사용자를 찾을 수 없습니다.");
+
+
+    private final HttpStatus httpStatus;
+    private final boolean isSuccess;
+    private final String code;
+    private final String message;
+}
---
+++ back-end/src/main/java/com/newstoss/global/handler/ResponseErrorEntity.java
@@ -0,0 +1,27 @@
+package com.newstoss.global.handler;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+
+@Data
+@Builder
+public class ResponseErrorEntity {
+    private int status;
+    private boolean isSuccess;
+    private String name;
+    private String code;
+    private String message;
+
+    public static ResponseEntity<ResponseErrorEntity> toResponseEntity(ErrorCode e) {
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(ResponseErrorEntity.builder()
+                        .status(e.getHttpStatus().value())
+                        .isSuccess(e.isSuccess())
+                        .name(e.name())
+                        .code(e.getCode())
+                        .message(e.getMessage())
+                        .build());
+    }
+}
