7c72eccb6788dbbf1262a872d27e5eba31b568a0
hyeongjun <gudwns1423@gmail.com>
2025-05-22 17:20:41 +0900

    [Feat] 증권 initDB 개발

--- back-end/src/main/java/com/newstoss/global/handler/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/newstoss/global/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.newstoss.global.handler;
 
+import com.newstoss.global.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +14,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ResponseErrorEntity> handleCustomException(CustomException e) {
         return ResponseErrorEntity.toResponseEntity(e.errorCode);
+    }
+
+    @ExceptionHandler(InvalidStockPriceFormatException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidStockPriceFormatException(InvalidStockPriceFormatException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("INVALID_STOCK_PRICE_FORMAT", e.getMessage()));
     }
 }
