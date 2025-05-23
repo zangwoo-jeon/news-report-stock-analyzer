afc2a7872423028aa0f1378bacfe82038eddd286
hyeongjun <gudwns1423@gmail.com>
2025-05-23 13:42:43 +0900

    handler

--- back-end/src/main/java/com/newstoss/global/handler/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/newstoss/global/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.newstoss.global.handler;
 
 import com.newstoss.global.response.ResponseErrorEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
